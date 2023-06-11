c69f25b0cf7522961c2dc4075931791285abda25
Kinshu-Learner <kinshurajgupta1@gmail.com>
2023-06-11 22:20:15 +0530

    Basic fix

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -55,7 +55,7 @@ function Navbar() {
       {/* To only display this menu when "nav" is true on smaller screens */}
       <ul className={`flex flex-col justify-center items-center md:hidden text-gray-400 absolute ${nav ? "h-screen" : "h-0"} w-full top-0 left-0 ${nav ? "translate-y-0" : "-translate-y-40"} bg-gradient-to-b from-black to-gray-800 z-[-1] text-3xl duration-300 ease-in-out`}>
         {links.map(({ id, link }) => (
-          <li key={id} className='hover:text-cyan-200 duration-200 hover:scale-105 cursor-pointer mx-4 capitalize py-4'>
+          <li key={id} className='hover:text-purple-500 duration-200 hover:scale-105 cursor-pointer mx-4 capitalize py-4'>
             <Link onClick={()=>SetNav(!nav)} to={link} smooth duration={500}>{link}</Link>
             </li>
         ))}
