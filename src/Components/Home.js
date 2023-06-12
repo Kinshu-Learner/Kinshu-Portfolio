4f9fb252e4cd951e067a6f63eea100e721b04a82
Kinshu-Learner <kinshurajgupta1@gmail.com>
2023-06-12 19:14:19 +0530

    Added a simple Typewriter animation

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import { React } from 'react'
 import mainpic from "../Assets/Main pic probably.png"
 import { Link } from 'react-scroll'
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
 
 function Home() {
+
+  const [text] = useTypewriter({
+    words: ['Frontend Developer.', 'Full Stack Developer.', 'Coder.'],
+    loop: {},
+    typeSpeed: 70,
+    deleteSpeed: 50
+  })
+  
   return (
     <div name='home' className='bg-gradient-to-b from-black via-black to-gray-800 h-screen '>
 
@@ -10,7 +19,11 @@ function Home() {
 
         <div className='flex flex-col justify-center h-full x-sm:mt-40 md:mt-0'>
 
-          <h2 className=' text-white text-4xl md:text-7xl text-bold py-4'>I'm a <span className='text-transparent bg-clip-text bg-gradient-to-tr from-indigo-500 to-purple-700'>Full Stack</span>  <span className='text-transparent bg-clip-text bg-gradient-to-tr from-cyan-500 to-blue-500'>Developer</span></h2>
+          <h2 className='h-28 md:h-44 text-white text-4xl md:text-7xl py-4'>I'm a  <span  className='text-transparent bg-clip-text bg-gradient-to-tr from-purple-500 to-sky-500'>
+            {text}
+            </span> 
+            <span><Cursor/></span>
+            </h2>
           <p className='pb-4 text-gray-400'>
             I love to build stuff using technologies like React, Tailwind, NodeJS, Express, Mongo etc. I also like learning and quickly adapting to new technologies, so I'm always learning something new. I'm also a coder, and have also worked on Python with MySQL in the past.
           </p>
